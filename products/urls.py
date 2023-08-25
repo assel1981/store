3bc56825719ec2254d88d7a04ce3b792a449a95c
Assel <Assel_rakisheva@mail.ru>
2023-08-25 14:29:56 +0600

    видео 53 часть 2

--- products/urls.py
+++ products/urls.py
@@ -19,10 +19,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-from products.views import products
+from products.views import products, basket_add
 
 app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
+    path('basket/add/<int:product_id>', basket_add, name='basket_add')
 ]
